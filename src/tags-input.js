c053fc120f3e49c099cd149f2402be9dbf0c4457
Jason Grier <jasongrier@MacBook-Pro.local>
2016-09-27 14:49:32 +0200

    Add option to delete a tag without confirmation.

--- src/tags-input.js
+++ src/tags-input.js
@@ -203,7 +203,8 @@ tagsInput.directive('tagsInput', function($timeout, $document, $window, $q, tags
                 keyProperty: [String, ''],
                 allowLeftoverText: [Boolean, false],
                 addFromAutocompleteOnly: [Boolean, false],
-                spellcheck: [Boolean, true]
+                spellcheck: [Boolean, true],
+                removeImmediately: [Boolean, false],
             });
 
             $scope.tagList = new TagList($scope.options, $scope.events,
@@ -422,7 +423,7 @@ tagsInput.directive('tagsInput', function($timeout, $document, $window, $q, tags
                     addKeys[KEYS.space] = options.addOnSpace;
 
                     shouldAdd = !options.addFromAutocompleteOnly && addKeys[key];
-                    shouldRemove = (key === KEYS.backspace || key === KEYS.delete) && tagList.selected;
+                    shouldRemove = (key === KEYS.backspace || key === KEYS.delete) && (tagList.selected || options.removeImmediately);
                     shouldEditLastTag = key === KEYS.backspace && scope.newTag.text().length === 0 && options.enableEditingLastTag;
                     shouldSelect = (key === KEYS.backspace || key === KEYS.left || key === KEYS.right) && scope.newTag.text().length === 0 && !options.enableEditingLastTag;
 
